e710fe9ba907dff31879369733b04d4b89d88d25
Mnemosyne-Grayswandir <hamormarygrizelle@gmail.com>
2023-04-11 09:37:07 +0800

    Finished Text Preprocessing class

--- aia/preprocessing/utils.py
+++ aia/preprocessing/utils.py
@@ -2,40 +2,99 @@
 
 import pandas as pd
 import numpy as np
-from abc import ABC, abstractclassmethod
+import os
+from abc import ABC, abstractmethod
 
 class Preprocess_Data(ABC): # acc to different file types
-    def __init__(self, data_path) -> None:
+    def __init__(self, data_path: str) -> None:
         """
         Load the DataFrame using pandas
         """ 
         try:
-            self.data_frame = pd.read_csv(data_path)
+            DIR = os.path.abspath(data_path)
+            self.data_frame = pd.read_csv(DIR)
+            #print(self.data_frame)
         except (OSError, FileNotFoundError, TypeError):
             print("File path does not exist")
             exit(-1)
 
-    def drop_columns(self):
+    def view(self):
+        print(self.data_frame)
+
+    @abstractmethod
+    def preprocess(self):
         pass
 
-    def drop_rows(self):
-        pass
+class Preprocess_Text(Preprocess_Data):
+    def __init__(self, data_path: str, *args) -> None:
+        super().__init__(data_path)
 
-    def dummy_vars(self):
-        pass
+        self.action = 0
 
-    def missing_data(self):
-        pass
+        if args:
+            if len(args) > 1:
+                self.action = args[0]
+                self.value = args[1]
+            else:
+                self.action = args[0]
+
+    def __drop_columns(self):
+        try:
+            self.data_frame.drop([self.value], axis=1)
+            return self.data_frame
+        except Exception as e:
+            print(e)
+            exit(-1)
+
+    def __drop_empty_cells(self):
+        self.data_frame = self.data_frame.dropna()
+        return self.data_frame
+
+    def __replace_values(self):
+        try:
+            if self.action == 3:
+                self.data_frame.fillna(self.value)
+            elif self.action == 4:
+                self.data_frame.replace(to_replace = np.nan, value = self.value)
+            return self.data_frame
+        except Exception as e:
+            print(e)
+            exit(-1)
+
+    def __clean(self):
+        # Lowercase
+        self.data_frame = self.data_frame.apply(lambda x: x.astype(str).str.lower())
+
+        # Remove extra space
+        for i in self.data_frame.columns:
+            if self.data_frame[i].dtype == 'object':
+                self.data_frame[i] = self.data_frame[i].map(str.strip)
+            else:
+                pass
+
+        # Remove emojis
+        self.data_frame.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))  
+
+        return self.data_frame
+
+    def preprocess(self):
+        if self.action == 1:
+            self.data_frame = self.__drop_columns()
+        elif self.action == 2:
+            self.data_frame = self.__drop_empty_cells()
+        elif self.action == 3 or self.action == 4:
+            self.data_frame = self.__replace_values()
+        self.data_frame = self.__clean()
+        #print(self.data_frame)
+
+        return super().preprocess()
+
+class Preprocess_Image(Preprocess_Data):
+    def __init__(self, data_path) -> None:
+        super().__init__(data_path)
 
     def clean(self):
         pass
 
-    # Class must do the following
-    # 1. Load data in Pandas
-    # 2. Drop columns that are not useful
-    # 3. Drop rows with missing values
-    # 4. Create dummy variables
-    # 5. Take care of missing data
-    # -- lower, remove spaces, remove numbers in text
-    # 6. Convert the data frame to NumPy
-    # 7. Divide the data set into training data and test data
+    def preprocess(self):
+        return super().preprocess()
--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-from aia import preprocessing
-from aia.preprocessing.utils import Preprocessing
+from aia.preprocessing.utils import Preprocess_Text
 
-from aia import visualization
-
-from aia import deep_learning
-from aia.deep_learning import computer_vision
-from aia.deep_learning import nlp
-from aia.deep_learning.computer_vision import detection
-from aia.deep_learning.computer_vision import utils
-from aia.deep_learning.nlp import sentiment
-from aia.deep_learning.nlp import utils
-
-from aia import machine_learning
-from aia.machine_learning import classification
-from aia.machine_learning import regression
-from aia.machine_learning.classification import svm
-from aia.machine_learning.classification import utils
-from aia.machine_learning.regression import linear_regression
-from aia.machine_learning.regression import utils
-
+df = Preprocess_Text("C:\\Users\\GRIZELLE\\GitHub Projects\\aia\\Dataset\\cereal.csv")
+df.preprocess()
+df.view()
